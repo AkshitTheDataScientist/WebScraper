aa90f04f34b87fed630b3b98fa4916b555903842
AkshitTheDataScientist <44747067+AkshitTheDataScientist@users.noreply.github.com>
2018-11-06 14:21:01 +0000

    Add files via upload

---
+++ fundrazr/fundrazr/pipelines.py
@@ -0,0 +1,5 @@
+
+
+class FundrazrPipeline(object):
+    def process_item(self, item, spider):
+        return item
---
+++ fundrazr/fundrazr/settings.py
@@ -0,0 +1,9 @@
+
+BOT_NAME = 'fundrazr'
+
+SPIDER_MODULES = ['fundrazr.spiders']
+NEWSPIDER_MODULE = 'fundrazr.spiders'
+
+
+
+ROBOTSTXT_OBEY = True
